92fc07ddd8fcd64aa37e18fceab1a73e427ffcc9
Yahor Makedon <emakedonw@gmail.com>
2019-03-16 23:20:39 +0300

    move db scripts to migration support

---
+++ src/main/resources/db/migration/V2__Add_system_data.sql
@@ -0,0 +1,49 @@
+-- Insert data to table Strategy
+INSERT INTO aistudenttesterdb.Strategy (StrategyName, ActiveFlag) VALUES ('SIMPLE', 'Y');
+INSERT INTO aistudenttesterdb.Strategy (StrategyName, ActiveFlag) VALUES ('MULTI', 'N');
+
+-- Insert data to table Mark with SIMPLE strategy
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 0, 0, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 1, 0, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 2, 1, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 3, 1, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 4, 2, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 5, 2, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 6, 3, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 7, 3, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 8, 4, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 9, 4, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 10, 5, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 11, 5, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 12, 6, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 13, 6, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 14, 7, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 15, 7, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 16, 8, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 17, 8, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 18, 9, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 19, 9, 'Y');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (1, 20, 10, 'Y');
+
+-- Insert data to table Mark with MULTI strategy
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 0, 0, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 1, 0, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 2, 0, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 3, 1, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 4, 1, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 5, 1, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 6, 2, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 7, 2, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 8, 3, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 9, 3, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 10, 4, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 11, 4, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 12, 5, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 13, 5, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 14, 6, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 15, 6, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 16, 7, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 17, 7, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 18, 8, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 19, 9, 'N');
+INSERT INTO aistudenttesterdb.Mark (StrategyID, NumberOfRightQuestions, Mark, ActiveFlag) VALUES (2, 20, 10, 'N');
